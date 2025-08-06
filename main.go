0ff78f4819e466c1f9eb19f111dcd1af26abfe88
zaater <almogtbanmr123@gmail.com>
2025-08-06 19:48:01 +0300

    Adam

---
+++ main.go
@@ -0,0 +1,50 @@
+package thegameoflife
+
+type State [][]bool
+
+var directions = [][]int{
+	{0,1},
+	{1,1},
+	{1,0},
+	{1,-1},
+	{0,-1},
+	{-1,-1},
+	{-1,0},
+	{-1,1},
+}
+
+//1 Any live cell with fewer than two live neighbours dies, as if by underpopulation.
+//2 Any live cell with two or three live neighbours lives on to the next generation.
+//3 Any live cell with more than three live neighbours dies, as if by overpopulation.
+//4 Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
+func PlayRound(st State) State {
+	for y, row := range st {
+		for x := range row{
+			ncount := countNeigbours(st, x, y)
+			if ncount < 2 {
+				st[y][x] = false
+				continue
+			}
+			if ncount == 3 {
+				st[x][y] = true
+				continue
+			}
+			if ncount > 3 {
+				st[y][x] = false
+				continue
+			}
+		}
+	}
+	return st
+}
+
+func countNeigbours(st State, x, y int) (count int ) {
+	for _, dir := range directions {
+		ny := y + dir[0]
+		nx := x + dir[1]
+		if st[ny][nx] {
+			count++
+		}
+	}
+	return count
+}
